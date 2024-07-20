0dc5d632d859f1463ef390ca6041211efa037ab4
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-07-20 13:20:38 +0100

    Removed name as primary key for ImageReference.

--- photo_viewer/photo_viewer/models.py
+++ photo_viewer/photo_viewer/models.py
@@ -7,11 +7,11 @@ class Photographer(models.Model):
         return f"{self.__class__.__name__}: {self.name}"
 
 class ImageReference(models.Model):
-    image_name = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
     date_created = models.DateTimeField()
     ownerId = models.ForeignKey(Photographer, on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.__class__.__name__}: {self.image_name}"
+        return f"{self.__class__.__name__}: {self.name}"
         
 class Album(models.Model):
     name = models.CharField(max_length=255)
