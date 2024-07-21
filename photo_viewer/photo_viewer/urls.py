fbfb73697bcc6a1dd805e4760d957841489a6d6d
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-07-21 12:08:43 +0100

    Account management.

--- photo_viewer/photo_viewer/urls.py
+++ photo_viewer/photo_viewer/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/', views.profile_view, name='profile'),
 ]
--- photo_viewer/photo_viewer/views.py
+++ photo_viewer/photo_viewer/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.views import View, generic
 from .models import Album
 
@@ -9,3 +13,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Album.objects.order_by('-date_created')[:MAX_ALBUMS]
+
+@login_required
+def profile_view(request):
+    return render(request, 'photo_viewer/profile.html')
